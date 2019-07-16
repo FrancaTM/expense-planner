9c31b9b75dc3fd8ae00e7982a3b690f966e16fd1
FrancaTM <francatm@outlook.com>
2019-07-16 19:04:30 -0300

    initial

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Planner'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Chart'),
+              color: Colors.blue,
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('List of TX'),
+          ),
+        ],
+      ),
+    );
+  }
+}
