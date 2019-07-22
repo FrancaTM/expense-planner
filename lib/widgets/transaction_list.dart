e8af139ece77b09d326b62149cac8ff14ecce6fd
FrancaTM <francatm@outlook.com>
2019-07-22 17:29:29 -0300

    reverse chart items order fix chart bars issue

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -56,52 +56,6 @@ class TransactionList extends StatelessWidget {
                   ),
                 );
               },
-              // children: transactions.map((tx) {
-              //   return Card(
-              //     child: Row(
-              //       children: <Widget>[
-              //         Container(
-              //           margin: EdgeInsets.symmetric(
-              //             vertical: 10,
-              //             horizontal: 15,
-              //           ),
-              //           decoration: BoxDecoration(
-              //             border: Border.all(
-              //               color: Colors.purple,
-              //               width: 2,
-              //             ),
-              //           ),
-              //           padding: EdgeInsets.all(10),
-              //           child: Text(
-              //             '\$ ${tx.amount}',
-              //             style: TextStyle(
-              //               fontWeight: FontWeight.bold,
-              //               color: Colors.purple,
-              //             ),
-              //           ),
-              //         ),
-              //         Column(
-              //           crossAxisAlignment: CrossAxisAlignment.start,
-              //           children: <Widget>[
-              //             Text(
-              //               tx.title,
-              //               style: TextStyle(
-              //                 fontWeight: FontWeight.bold,
-              //                 fontSize: 18,
-              //               ),
-              //             ),
-              //             Text(
-              //               DateFormat().format(tx.date),
-              //               style: TextStyle(
-              //                 color: Colors.grey,
-              //               ),
-              //             ),
-              //           ],
-              //         ),
-              //       ],
-              //     ),
-              //   );
-              // }).toList(),
             ),
     );
   }
