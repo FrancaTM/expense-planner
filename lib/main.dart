d25547cda0781b3c354b9223cacfd5f9144527a9
FrancaTM <francatm@outlook.com>
2019-07-21 22:16:02 -0300

    add 'no transactions' indicator

--- lib/main.dart
+++ lib/main.dart
@@ -44,78 +44,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'Sample',
-      amount: 99.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Another sample',
-      amount: 12.13,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Another sample',
-      amount: 12.13,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Another sample',
-      amount: 12.13,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Another sample',
-      amount: 12.13,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Another sample',
-      amount: 12.13,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Another sample',
-      amount: 12.13,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Another sample',
-      amount: 12.13,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Another sample',
-      amount: 12.13,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Another sample',
-      amount: 12.13,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Another sample',
-      amount: 12.13,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Another sample',
-      amount: 12.13,
-      date: DateTime.now(),
-    ),
+    // Transaction(
+    //   id: 't1',
+    //   title: 'Sample',
+    //   amount: 99.99,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Another sample',
+    //   amount: 12.13,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   void _addNewTransaction(String txTitle, double txAmount) {
